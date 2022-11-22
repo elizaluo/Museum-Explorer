4f7f88b1a05b508f6e4d07e2d11e2f345a0fc456
roshni-v <34416927+roshni-v@users.noreply.github.com>
2022-11-22 15:01:27 -0800

    working object creation for people

--- musei/Data/Person.cs
+++ musei/Data/Person.cs
@@ -8,18 +8,21 @@ namespace musei.Data
 {
     public class Person
     {
-        private string name;
-        private string emailAddress;
-        private string optionalSpecialAccomodations;
-        public Person(string name, string emailAddress, string optionalSpecialAccomodations)
+        public int count;
+        public string name;
+        public string emailAddress;
+        public string optionalSpecialAccomodations;
+        public Person(int count, string name, string emailAddress, string optionalSpecialAccomodations)
         {
+            this.count = count;
             this.name = name;
             this.emailAddress = emailAddress;
             this.optionalSpecialAccomodations = optionalSpecialAccomodations;
         }
 
-        public Person ()
+        public Person (int count)
         {
+            this.count = count;
             this.name = null;
             this.emailAddress=null;
             this.optionalSpecialAccomodations = null;
@@ -29,13 +32,5 @@ namespace musei.Data
         { 
             return "PERSON-> Name: " + this.name + "; Email Address: " + this.emailAddress + "; Special Accomodations (Optional): " + this.optionalSpecialAccomodations + "\n";
         }
-
-        public string getName() { return name; }
-        public string getEmailAddress() { return emailAddress; }
-        public string getOptionalSpecialAccomodations() { return optionalSpecialAccomodations; }
-
-        public void setName(string name) { this.name = name; }
-        public void setEmailAddress(string emailAddress) { this.emailAddress = emailAddress; }
-        public void setOptionalSpecialAccomodations(string optionalSpecialAccomodations) { this.optionalSpecialAccomodations = optionalSpecialAccomodations; }
     }
 }
