ec95dc3833c31b94f5632129707dc302111d37b3
Arden Ozdere <ardenozdere@gmail.com>
2022-11-25 19:36:53 +0100

    linking data between event and museum pages

---
+++ musei/Data/Session.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace musei.Data
+{
+	public static class Session
+	{
+		static public Event currentEvent;
+		static public Museum currentMuseum;
+
+
+		static Session()
+		{
+		}
+
+
+	}
+}
+
